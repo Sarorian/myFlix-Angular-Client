cbe42ec31734f4de1a769cea6f95a3f0136923c1
Sarorian <shamowen5@gmail.com>
2024-01-08 21:21:49 -0700

    fix: wrong function name

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -144,29 +144,13 @@ export class FetchApiDataService {
       return throwError(
       'Something bad happened; please try again later.');
     }
-  }
-
-
-
-export class UserRegistrationService {
-  constructor(private http: HttpClient) {
-  }
 
   public userRegistration(userDetails: any): Observable<any> {
-    console.log(userDetails);
-    return this.http.post(apiUrl + 'users', userDetails).pipe(
-      catchError(this.handleError)
-    );
+      console.log(userDetails);
+      return this.http.post(apiUrl + 'users', userDetails).pipe(
+        catchError(this.handleError)
+      );
+    }
+
   }
 
-  private handleError(error: HttpErrorResponse): any {
-    if (error.error instanceof ErrorEvent) {
-      console.error('Some error occurred:', error.error.message);
-    } else {
-      console.error(
-        `Error Status code ${error.status}, ` +
-        `Error body is: ${error.error}`);
-    }
-    return throwError('Something bad happened; please try again later.');
-  }
-}
--- src/app/user-registration-form/user-registration-form.component.ts
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 
 import { MatDialogRef } from '@angular/material/dialog';
 
-import { UserRegistrationService } from '../fetch-api-data.service';
+import { FetchApiDataService } from '../fetch-api-data.service';
 
 import { MatSnackBar } from '@angular/material/snack-bar';
 
@@ -16,7 +16,7 @@ export class UserRegistrationFormComponent {
   @Input() userData = { username: '', password: '', email: '', birthday: ''};
 
   constructor(
-    public fetchApiData: UserRegistrationService,
+    public fetchApiData: FetchApiDataService,
     public dialogRef: MatDialogRef<UserRegistrationFormComponent>,
     public snackBar: MatSnackBar
   ) { }
